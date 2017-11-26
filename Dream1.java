6aed52f56f15439f9806e6a9d97c4bcc25a48841
FreezeAutomation <derideriduke@yahoo.com>
2017-11-26 15:33:32 -0500

    added Dream1.class Dream1.java

---
+++ Dream1.java
@@ -0,0 +1,12 @@
+package dreamJava;
+
+public class Dream1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I can achieve anything if I put my mind to it");
+
+	}
+
+}
