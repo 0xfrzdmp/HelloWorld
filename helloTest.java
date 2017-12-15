95c49061af4372612935f6bb88d057f0bc7705cf
FreezeAutomation <derideriduke@yahoo.com>
2017-12-15 12:03:56 -0500

    modified helloTest.java

--- helloTest.java
+++ helloTest.java
@@ -5,8 +5,12 @@ public class helloTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1;i<=10;i++) {
+		for(int i=1;i<=5;i++) {
 			System.out.println("Hello World .."+i);
+		}
+		
+		for(int i=1;i<=5;i++){
+		    System.out.println("Merry Christmas.."+i)
 		}
 
 	}
