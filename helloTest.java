575f4811b660f30a757836883540254f9908ab7f
FreezeAutomation <derideriduke@yahoo.com>
2017-12-17 14:49:21 -0500

    modified helloTest.java

--- helloTest.java
+++ helloTest.java
@@ -9,9 +9,14 @@ public class helloTest {
 			System.out.println("Hello World .."+i);
 		}
 		
+<<<<<<< HEAD
 		for(int i=1;i<=5;i++){
 		    System.out.println("Merry Christmas.."+i)
 		}
+=======
+		for (int i=1;i<=2;i++)
+			System.out.println("Merry Christmass.."+i);
+>>>>>>> modified helloTest.class helloTest.java
 
 	}
 
